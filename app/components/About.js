cc7e64efc5f55c4dec289935c51de954f0031333
NaTaLiYa01 <nmoskalenko911@gmail.com>
2023-08-18 14:26:28 +0300

    adds react-scroll

--- app/components/About.js
+++ app/components/About.js
@@ -1,25 +1,27 @@
 export default function About() {
   return (
-    <section id="About" className="min-h-screen" >
-      <h2>Who we are</h2>
-      <p>
-        a team of enthusiasts who are fully committed to the mission of creating unforgettable and
-        extraordinary trips to the most beautiful parts of the Carpathians. Our goal is not just to
-        show you the natural wonders of the mountains, but to provide you with a deep immersion in
-        their magical atmosphere.
-      </p>
-      <p>
-        We believe that nature has the power to inspire, strengthen character and provide new
-        perspectives. Therefore, each of our tours is aimed at unlocking your potential, enriching
-        your spiritual world and creating unforgettable memories.
-      </p>
-      <p>From vacationers</p>
-      <p>to active travelers</p>
-      <p>we have a tour for everyone.</p>
-      <p>
-        We use methods that are time-tested and proven. Our expert guides with in-depth knowledge of
-        the Carpathian landscapes lead you safely through the mysteries of these mountains.
-      </p>
+    <section id="About" className="min-h-screen">
+      <div>
+        <h2>Who we are</h2>
+        <p>
+          a team of enthusiasts who are fully committed to the mission of creating unforgettable and
+          extraordinary trips to the most beautiful parts of the Carpathians. Our goal is not just
+          to show you the natural wonders of the mountains, but to provide you with a deep immersion
+          in their magical atmosphere.
+        </p>
+        <p>
+          We believe that nature has the power to inspire, strengthen character and provide new
+          perspectives. Therefore, each of our tours is aimed at unlocking your potential, enriching
+          your spiritual world and creating unforgettable memories.
+        </p>
+        <p>From vacationers</p>
+        <p>to active travelers</p>
+        <p>we have a tour for everyone.</p>
+        <p>
+          We use methods that are time-tested and proven. Our expert guides with in-depth knowledge
+          of the Carpathian landscapes lead you safely through the mysteries of these mountains.
+        </p>
+      </div>
     </section>
   );
 }
--- app/components/Header.js
+++ app/components/Header.js
@@ -1,13 +1,63 @@
+'use client';
+
+import { Link } from 'react-scroll';
+
 export default function Header() {
   return (
     <header className="flex justify-between">
       <div>CarpTravel</div>
       <nav className="flex gap-x-14">
-        <a href="#About">About</a>
-        <a href="#Services">Services</a>
-        <a href="#Career">Career</a>
-        <a href="#Gallery">Gallery</a>
-        <a href="#Contacts">Contacts</a>
+        <Link
+          // activeClass="active"
+          to="About"
+          spy={true}
+          smooth={true}
+          offset={50}
+          duration={500}
+        >
+          About
+        </Link>
+
+        <Link
+          // activeClass="active"
+          to="Services"
+          spy={true}
+          smooth={true}
+          offset={50}
+          duration={500}
+        >
+          Services
+        </Link>
+        <Link
+          // activeClass="active"
+          to="Career"
+          spy={true}
+          smooth={true}
+          offset={50}
+          duration={500}
+        >
+          Career
+        </Link>
+        <Link
+          // activeClass="active"
+          to="Gallery"
+          spy={true}
+          smooth={true}
+          offset={50}
+          duration={500}
+        >
+          Gallery
+        </Link>
+        <Link
+          // activeClass="active"
+          to="Contacts"
+          spy={true}
+          smooth={true}
+          offset={50}
+          duration={500}
+        >
+          Contacts
+        </Link>
       </nav>
     </header>
   );
