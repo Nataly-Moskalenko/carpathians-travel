380ada0aaf963d219f5cc79095b2c60a97353328
NaTaLiYa01 <nmoskalenko911@gmail.com>
2023-08-19 15:08:26 +0300

    fixes contacts form validation

--- app/components/FormContacts.js
+++ app/components/FormContacts.js
@@ -31,9 +31,30 @@ export default function FormContacts() {
             name="fullName"
             placeholder="John Smith"
             type="text"
-            className="text-[20px] font-extralight leading-6 bg-gray"
+            className="text-[20px] font-extralight leading-6 bg-gray w-[293px]"
           />
-          {errors.fullName && <p>Incorrect name</p>}
+          {errors.fullName && (
+            <p className="flex items-center justify-end">
+              <svg                
+                width="18"
+                height="18"
+                viewBox="0 0 18 18"
+                fill="none"
+              >
+                <path
+                  d="M4.375 4.375L13.625 13.625M13.625 4.375L4.375 13.625"
+                  stroke="#FF5757"
+                  stroke-width="0.5"
+                  stroke-miterlimit="10"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                />
+              </svg>
+              <span className="text-orange text-[12px] font-extralight leading-6 tracking-[2.4px]">
+                Incorrect name
+              </span>
+            </p>
+          )}
         </div>
         <div>
           <label htmlFor="email" className="text-[12px] font-extralight leading-6 tracking-[2.4px]">
@@ -42,14 +63,35 @@ export default function FormContacts() {
           <input
             {...register('email', {
               required: true,
-              validate: (input) => isEmail(input),
+              pattern: /^[a-zA-Z0-9]+@[a-zA-Z]+.[a-zA-Z]{2,3}$/,
             })}
             name="email"
             type="email"
             placeholder="johnsmith@email.com"
-            className="text-[20px] font-extralight leading-6 bg-gray"
+            className="text-[20px] font-extralight leading-6 bg-gray w-[293px]"
           />
-          {errors.email && <p>Invalid email</p>}
+          {errors.email && (
+            <p className="flex items-center justify-end">
+              <svg                
+                width="18"
+                height="18"
+                viewBox="0 0 18 18"
+                fill="none"
+              >
+                <path
+                  d="M4.375 4.375L13.625 13.625M13.625 4.375L4.375 13.625"
+                  stroke="#FF5757"
+                  stroke-width="0.5"
+                  stroke-miterlimit="10"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                />
+              </svg>
+              <span className="text-orange text-[12px] font-extralight leading-6 tracking-[2.4px]">
+                Invalid email
+              </span>
+            </p>
+          )}
         </div>
       </div>
       <label
